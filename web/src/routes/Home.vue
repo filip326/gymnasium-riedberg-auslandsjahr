632059c1a866f4aebbf244abe9a8fb0425d5f751
heeecker <89743412+filip326@users.noreply.github.com>
2024-06-30 17:28:23 +0200

    add shadcn and buttons as wanted

--- web/src/routes/Home.vue
+++ web/src/routes/Home.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import Button from "@/components/ui/button/Button.vue";
+</script>
 
 <template>
     <div class="flex justify-center flex-col items-center">
@@ -10,6 +12,7 @@
         <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Auslandsaufenthalt</h1>
         <h2 class="scroll-m-20 text-2xl font-bold tracking-tight lg:text-3xl">am Gymnasium Riedberg</h2>
         <p>Interessiert an einem Auslandsaufenthalt? Bitte fülle das Formular gemeinsam mit deinen Eltern aus.</p>
+        <Button> Formular ausfüllen </Button>
     </div>
 </template>
 
